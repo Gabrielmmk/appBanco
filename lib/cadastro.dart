a874d4d14f3fca3f9d375c4603a23f3f2fa499bb
Gabriel Medeiros <gabrielmmk087@gmail.com>
2024-04-23 12:36:33 -0300

    Tela de cadastro concluída

--- lib/cadastro.dart
+++ lib/cadastro.dart
@@ -1,16 +1,102 @@
 import 'package:flutter/material.dart';
 
+
+
 class CadastroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Página de Cadastro'),
+        title: Text('Cadastre-se'),
       ),
-      body: const Center(
-        child: Text(
-          'Esta é a página de cadastro!',
-          style: TextStyle(fontSize: 20),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text(
+                'Nome:',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Digite seu nome',
+                ),
+                validator: (value){
+                  if (value == null || value.isEmpty){
+                    return 'Por favor, insira seu nome';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Email:',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Digite seu email',
+                ),
+                validator: (value){
+                  if (value == null || value.isEmpty){
+                    return 'Por favor, insira seu email';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'CPF:',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Digite seu CPF',
+                ),
+                validator: (value){
+                  if (value == null || value.isEmpty){
+                    return 'Por favor, insira seu CPF';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Senha:',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: 'Digite sua senha',
+                ),
+                validator: (value){
+                  if (value == null || value.isEmpty){
+                    return 'Por favor, insira sua senha';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Implemente a lógica de envio do formulário
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                  foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(7),
+                    ),
+                  ),
+                ),
+                child: const Text('Cadastrar'),
+              ),
+            ],
+          ),
         ),
       ),
     );
