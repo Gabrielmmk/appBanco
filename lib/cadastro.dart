b72171e0dfec25e6f7036bc4772534d1be9efe62
Gabriel Medeiros <gabrielmmk087@gmail.com>
2024-04-23 13:37:00 -0300

    Adicionado validação no formuário de cadastro

--- lib/cadastro.dart
+++ lib/cadastro.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
-
-
 class CadastroPage extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
+  CadastroPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:
-        const Text(
+        title: const Text(
           'Cadastro',
           style: TextStyle(
             fontWeight: FontWeight.bold,
@@ -25,90 +26,106 @@ class CadastroPage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const Text(
-                'Nome:',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Digite seu nome',
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text(
+                  'Nome:',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                 ),
-                validator: (value){
-                  if (value == null || value.isEmpty){
-                    return 'Por favor, insira seu nome';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'Email:',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Digite seu email',
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Digite seu nome',
+                  ),
+                  validator: (value){
+                    if (value == null || value.isEmpty){
+                      return 'Por favor, insira seu nome';
+                    }
+                    return null;
+                  },
                 ),
-                validator: (value){
-                  if (value == null || value.isEmpty){
-                    return 'Por favor, insira seu email';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'CPF:',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Digite seu CPF',
+                const SizedBox(height: 20),
+                const Text(
+                  'Email:',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                 ),
-                validator: (value){
-                  if (value == null || value.isEmpty){
-                    return 'Por favor, insira seu CPF';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'Senha:',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              TextFormField(
-                obscureText: true,
-                decoration: const InputDecoration(
-                  hintText: 'Digite sua senha',
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Digite seu email',
+                  ),
+                  validator: (value){
+                    if (value == null || value.isEmpty){
+                      return 'Por favor, insira seu email';
+                    }
+                    return null;
+                  },
                 ),
-                validator: (value){
-                  if (value == null || value.isEmpty){
-                    return 'Por favor, insira sua senha';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  // Implemente a lógica de envio do formulário
-                },
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-                  foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(7),
+                const SizedBox(height: 20),
+                const Text(
+                  'CPF:',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Digite seu CPF',
+                  ),
+                  keyboardType: TextInputType.number,
+                  validator: (value){
+                    if (value == null || value.isEmpty){
+                      return 'Por favor, insira seu CPF';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Senha:',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: 'Digite sua senha',
+                  ),
+                  validator: (value){
+                    if (value == null || value.isEmpty){
+                      return 'Por favor, insira sua senha';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // Verifica todos os campos do formulário
+                    if (_formKey.currentState!.validate()) {
+                      // Implemente a lógica de envio do formulário caso o formulário esteja certo
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            backgroundColor: Colors.blue,
+                            content: Text(
+                              'Você está cadastrado!',
+                              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                            )
+                        ),
+                      );
+                    }
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(7),
+                      ),
                     ),
                   ),
+                  child: const Text('Cadastrar'),
                 ),
-                child: const Text('Cadastrar'),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
